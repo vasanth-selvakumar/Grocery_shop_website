26d28fb94a83b39432918d24fcab5b5b9222535b
Vasanth <selvakumarvasanth80@gmail.com>
2026-07-28 12:57:23 +0530

    Fix email syntax

--- grocery_website/settings.py
+++ grocery_website/settings.py
@@ -140,8 +140,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER=selvakumarvasanth80@gmail.com
-EMAIL_HOST_PASSWORD=vpyi zecs gull ndvl 
+EMAIL_HOST_USER="selvakumarvasanth80@gmail.com"
+EMAIL_HOST_PASSWORD="vpyi zecs gull ndvl"
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 LOGIN_URL = 'login'
