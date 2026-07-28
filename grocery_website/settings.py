690f3fdad2b54a50ecba74f47d6ccb2c46530360
Vasanth <selvakumarvasanth80@gmail.com>
2026-07-28 14:27:12 +0530

    Update Gmail app password

--- grocery_website/settings.py
+++ grocery_website/settings.py
@@ -141,7 +141,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER="selvakumarvasanth80@gmail.com"
-EMAIL_HOST_PASSWORD="vpyi zecs gull ndvl"
+EMAIL_HOST_PASSWORD="klrm ljbo simz lfxn"
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 LOGIN_URL = 'login'
